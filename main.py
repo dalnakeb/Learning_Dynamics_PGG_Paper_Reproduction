b4fcb041fc8afadb41f2ae13085d84c9f43e5d3d
derar <deraralnakeb@gmail.be>
2023-12-18 13:55:48 +0100

    Init

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+Authors: Alnakeb Derar, Caiola Ludovica, Correa Catalina, Andres Antelo
+Description: Reproducing the results and experimentation conducted in the scientific paper: "Social Diversity Promotes
+The Emergence Of Cooperation In Public Goods Games".
+"""
+
+if __name__ == "__main__":
+    print("Yo")
